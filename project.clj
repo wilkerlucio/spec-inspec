081f5968b1d75cf450fa50c5bc5635b44a57dbe4
Wilker Lucio <wilkerlucio@gmail.com>
2017-10-08 12:07:52 -0300

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.wsscode.spec-inspec "0.1.0-SNAPSHOT"
+(defproject com.wsscode.spec-inspec "1.0.0-alpha1-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
